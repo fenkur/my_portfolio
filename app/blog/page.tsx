64dc66f5d716f9bf4947475271df9adb99b40048
Fenky Wah <fenkwah15@gmail.com>
2025-05-13 11:53:28 -0500

    ESLint Rule

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,6 @@
 import Footer from "@/components/Footer"
 import NavBar from "@/components/NavBar"
+
 export default function Home() {
     return (
         <>
@@ -8,17 +9,18 @@ export default function Home() {
             <section className="mx-auto max-w-screen-sm px-5 py-12">
                 <div className="space-y-6">
                     <h1 className="text-1xl font-bold">Blog / Yap</h1>
-                    <p className="text-white leading-relaxed">This will be a dedicated page for my background, interest, and other stuff I'm interested in. 
-                        My interest for tech started when I was a kid. I was always playing games on Roblox or Facebook during the 2010's and it got me into hacking.
+                    <p className="text-white leading-relaxed">
+                        This will be a dedicated page for my background, interest, and other stuff I&apos;m interested in. 
+                        My interest for tech started when I was a kid. I was always playing games on Roblox or Facebook during the 2010&apos;s and it got me into hacking.
                         I started watching tutorials on YouTube on how to get those hacks and it taught me how to use a tool called Cheat Engine. Looking back at it,
                         I realized how it was able to do all those things because during my first programming class, we were taught about data types such as integers, floats, etc. 
-                        and Cheat Engine allowed you to change certain memory to whatever you want. I haven't learned Assembly but the GUI for Cheat Engine look similiar so I understand
+                        and Cheat Engine allowed you to change certain memory to whatever you want. I haven&apos;t learned Assembly but the GUI for Cheat Engine looked similar so I understand
                         now how it was able to do all hacks back then. Another thing that got me into tech was my friends and family. I come from a family of immigrants so my parents
-                        didn't speak English very well. I was able to learn English through school and YouTube tutorials so they would come to me for tech related stuff. I hope to use my
+                        didn&apos;t speak English very well. I was able to learn English through school and YouTube tutorials so they would come to me for tech related stuff. I hope to use my
                         knowledge to help others or just make whatever I want that comes to mind. 
                     </p>
                     <p className="text-white leading-relaxed">
-                        It's currently summer right now so I'm trying to find a job and build projects along the way. I'm also trying to get an AWS certificate along the way.
+                        It&apos;s currently summer right now so I&apos;m trying to find a job and build projects along the way. I&apos;m also trying to get an AWS certificate along the way.
                     </p>
                 </div>
             </section>
--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,54 @@
 import Navbar from "@/components/NavBar";
 import Link from 'next/link';
-import Contacts from '@/components/Contacts'
+import Contacts from '@/components/Contacts';
 import Footer from "@/components/Footer";
-
 
 export default function Home() {
   return (
     <>
-    <Navbar />
-    <div className="bg-black text-white min-h-screen px-8">
-      <section className="mx-auto max-w-screen-sm px-5 py-12">
-        <div className="space-y-6">
-          <h1 className="text-1xl font-bold">Hi, I'm Fenky</h1>
-          <p className="text-white leading-relaxed">I'm a student at the University of Minnesota majoring in IT Infrastructure. 
-            Even though I'm not a Computer Science student, I like to code and learn computer science related topics in my spare time. 
-            Feel free to connect with me on LinkedIn or send me an email.</p>
-        </div>
-      </section>
-
-      <section className="mx-auto max-w-screen-sm px-5 py-12">
-        <div className="space-y-6">
-          <h1 className="text-1xl font-bold" id="projects">Projects</h1>
-          <Link href="https://github.com/fenkur/study-website">
-            <h1>Study Tracker Website: </h1>
-          </Link>
-          <p>I wanted to practice frontend and backend development so I tried making a simple full-stack web application. It was built using Next.js (TypeScript), Tailwind CSS, Express.js, and MongoDB. </p>
-          
-          <Link href="https://github.com/fenkur/Subscription-Tracker">
-            <h1>Subscription Tracker: </h1>
-          </Link>
-          <p>A project I made following a YouTube tutorial on Backend Development. It taught me things such as building RESTful APIs, JWT Authentication, Security, Database Modeling with MongoDB, etc.</p>
-
-          <div>
-            <h1>Other Projects: </h1>
-            <p>I will try my best to add more projects in the future!!</p>
+      <Navbar />
+      <div className="bg-black text-white min-h-screen px-8">
+        <section className="mx-auto max-w-screen-sm px-5 py-12">
+          <div className="space-y-6">
+            <h1 className="text-1xl font-bold">Hi, I&apos;m Fenky</h1>
+            <p className="text-white leading-relaxed">
+              I&apos;m a student at the University of Minnesota majoring in IT Infrastructure. 
+              Even though I&apos;m not a Computer Science student, I like to code and learn computer science related topics in my spare time. 
+              Feel free to connect with me on LinkedIn or send me an email.
+            </p>
           </div>
-        </div>
-      </section>
-      
-      <Contacts />
-      <Footer />
-    </div>
+        </section>
+
+        <section className="mx-auto max-w-screen-sm px-5 py-12">
+          <div className="space-y-6">
+            <h1 className="text-1xl font-bold" id="projects">Projects</h1>
+
+            <Link href="https://github.com/fenkur/study-website">
+              <h1>Study Tracker Website: </h1>
+            </Link>
+            <p>
+              I wanted to practice frontend and backend development so I tried making a simple full-stack web application. 
+              It was built using Next.js (TypeScript), Tailwind CSS, Express.js, and MongoDB.
+            </p>
+
+            <Link href="https://github.com/fenkur/Subscription-Tracker">
+              <h1>Subscription Tracker: </h1>
+            </Link>
+            <p>
+              A project I made following a YouTube tutorial on Backend Development. 
+              It taught me things such as building RESTful APIs, JWT Authentication, Security, Database Modeling with MongoDB, etc.
+            </p>
+
+            <div>
+              <h1>Other Projects: </h1>
+              <p>I will try my best to add more projects in the future!!</p>
+            </div>
+          </div>
+        </section>
+
+        <Contacts />
+        <Footer />
+      </div>
     </>
   );
 }
